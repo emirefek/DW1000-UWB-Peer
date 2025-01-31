cce26a3e71eeacbfbaad58a6d682f9fb663b3dc3
emirefek <emirefek@gmail.com>
2025-01-31 14:43:09 +0300

    wave types added

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <DW1000Ng.hpp>
-#include <WiFi.h>
 #include "utils.h"
 #include <vector>
 #include <string>
+#include <cstring>
 
 // CONNECTION PINS BEGIN
 #define SPI_SCK 18
@@ -17,27 +17,73 @@ const uint8_t PIN_SS = 4;   // spi select pin
 // CONNECTION PINS END
 
 // Extended Unique Identifier register. 64-bit device identifier. Register file: 0x01
-char EUI[] = "AA:BB:CC:DD:EE:FF:00:00";
-volatile uint32_t beacon_rate = 100;
-volatile uint32_t last_beacon = 0;
+char EUID[] = "AA:BB:CC:DD:EE:FF:00:00";
 
-enum device_type
+enum CarrierType
 {
   FORKLIFT,
-  DOOR,
-  LIFT
+  DOOR
 };
-device_type myDeviceType = FORKLIFT;
+CarrierType thisCarrierType = FORKLIFT;
 
-struct DeviceInfo
+enum WaveType
 {
-  device_type type;
-  String eui;
-  uint32_t timestamp;
+  BLINK,
+  ANSWER,
+  RESULT
 };
-std::vector<DeviceInfo> metadataBuffer;
 
-unsigned long lastMetadataPrintTime = 0;
+struct BlinkWave
+{
+  WaveType type = WaveType::BLINK;
+  String euid;
+  uint32_t departure_time;
+  CarrierType carrier;
+};
+
+struct AnswerWave
+{
+  WaveType type = WaveType::ANSWER;
+  String euid;
+  String initiator_euid;
+  uint32_t departure_time;
+  uint32_t taxi_time;
+  CarrierType carrier;
+};
+
+struct ResultWave
+{
+  WaveType type = WaveType::RESULT;
+  String initiator_euid;
+  String responder_euid;
+  float calculated_distance;
+};
+
+struct WaveMessage
+{
+  WaveType type;
+  union
+  {
+    BlinkWave blink;
+    AnswerWave answer;
+    ResultWave result;
+  };
+
+  // Constructor for BlinkWave
+  WaveMessage(const BlinkWave &bw) : type(WaveType::BLINK), blink(bw) {}
+
+  // Constructor for AnswerWave
+  WaveMessage(const AnswerWave &aw) : type(WaveType::ANSWER), answer(aw) {}
+
+  // Constructor for ResultWave
+  WaveMessage(const ResultWave &rw) : type(WaveType::RESULT), result(rw) {}
+
+  // Destructor - Needed for union
+  ~WaveMessage() {}
+};
+
+const uint16_t blinkRate = 300;
+uint32_t lastBlinkTime = 0;
 
 device_configuration_t DEFAULT_CONFIG = {
     false,
@@ -52,114 +98,10 @@ device_configuration_t DEFAULT_CONFIG = {
     PreambleLength::LEN_256,
     PreambleCode::CODE_3};
 
-void addToMetadataBuffer(device_type type, const String &eui, uint32_t timestamp)
-{
-  for (auto &device : metadataBuffer)
-  {
-    if (device.eui == eui)
-    {
-      device.timestamp = timestamp; // Update the timestamp if the EUI matches
-      return;
-    }
-  }
-  // If the EUI is not found, add a new entry
-  DeviceInfo device = {type, eui, timestamp};
-  metadataBuffer.push_back(device);
-}
-
-void printAndClearMetadataBuffer()
-{
-  if (millis() - lastMetadataPrintTime > 1000)
-  {
-    Serial.println("[INFO]Collected Beacons:");
-    for (const auto &device : metadataBuffer)
-    {
-      Serial.print("[INFO]Device Type: ");
-      switch (device.type)
-      {
-      case FORKLIFT:
-        Serial.print("FORKLIFT");
-        break;
-      case DOOR:
-        Serial.print("DOOR");
-        break;
-      case LIFT:
-        Serial.print("LIFT");
-        break;
-      }
-      Serial.print(" | EUI: ");
-      Serial.print(device.eui);
-      Serial.print(" | Timestamp: ");
-      Serial.println(device.timestamp);
-    }
-    metadataBuffer.clear();
-    lastMetadataPrintTime = millis();
-  }
-}
-
-void transmitMeta()
-{
-  uint8_t message[13]; // 1 byte for device type, 8 bytes for EUI, 4 bytes for timestamp
-  uint32_t timestamp = millis();
-
-  // Construct the message
-  message[0] = myDeviceType;                         // 1 byte for device type
-  memcpy(&message[1], EUI, 8);                       // 8 bytes for EUI
-  memcpy(&message[9], &timestamp, sizeof(uint32_t)); // 4 bytes for timestamp
-
-  DW1000Ng::setTransmitData(message, sizeof(message));
-  DW1000Ng::startTransmit(TransmitMode::IMMEDIATE);
-
-  unsigned long startTime = millis();
-  const unsigned long timeout = 10; // 200 ms timeout
-
-  while (!DW1000Ng::isTransmitDone())
-  {
-    if (millis() - startTime > timeout)
-    {
-      DW1000Ng::clearTransmitStatus();
-      return;
-    }
-    yield();
-  }
-
-  last_beacon = millis();
-  DW1000Ng::clearTransmitStatus();
-}
-
-void collectMeta()
-{
-  DW1000Ng::startReceive();
-  unsigned long startTime = millis();
-  const unsigned long timeout = 10; // 200 ms timeout
-
-  while (!DW1000Ng::isReceiveDone())
-  {
-    if (millis() - startTime > timeout)
-    {
-      // Serial.println("[ERROR]Receive timeout");
-      // DW1000Ng::reset();
-      DW1000Ng::clearReceiveStatus();
-      DW1000Ng::clearReceiveFailedStatus();
-      DW1000Ng::clearReceiveTimeoutStatus();
-      return;
-    }
-    yield();
-  }
-
-  DW1000Ng::clearReceiveStatus();
-  uint8_t message[13];
-  DW1000Ng::getReceivedData(message, sizeof(message));
-
-  device_type type = static_cast<device_type>(message[0]);
-  char eui[9];
-  memcpy(eui, &message[1], 8);
-  eui[8] = '\0'; // Null-terminate the EUI string
-  uint32_t timestamp;
-  memcpy(&timestamp, &message[9], sizeof(uint32_t));
-
-  addToMetadataBuffer(type, String(eui), timestamp);
-}
+void transmitWaveMessage(const WaveMessage &message);
+void receiveWaveMessage(WaveMessage &message);
+void serializeWaveMessage(const WaveMessage &message, uint8_t *buffer, size_t &length);
+void deserializeWaveMessage(const uint8_t *buffer, size_t length, WaveMessage &message);
 
 void setup()
 {
@@ -197,20 +139,159 @@ void setup()
   Serial.print("[INFO]Device mode: ");
   Serial.println(msg);
 
-  generateUniqueId(EUI);
-  Serial.print("[INFO]Generated Unique Device ID: ");
-  Serial.println(EUI);
+  generateUniqueId(EUID);
+  Serial.print("[INFO]Unique Device ID: ");
+  Serial.println(EUID);
 }
 
 void loop()
 {
-  if (millis() - last_beacon > beacon_rate)
+  // Blink every 300ms
+  if (millis() - lastBlinkTime > blinkRate)
   {
-    transmitMeta();
+    lastBlinkTime = millis();
+    BlinkWave bw;
+    bw.euid = EUID;
+    bw.departure_time = millis();
+    bw.carrier = thisCarrierType;
+
+    WaveMessage message(bw);
+    transmitWaveMessage(message);
   }
-  else
+
+  // create a WaveMessage object with the type BLINK
+}
+
+void transmitWaveMessage(const WaveMessage &message)
+{
+  uint8_t buffer[128];
+  size_t length;
+  serializeWaveMessage(message, buffer, length);
+
+  DW1000Ng::setTransmitData(buffer, length);
+  DW1000Ng::startTransmit(TransmitMode::IMMEDIATE);
+
+  unsigned long startTime = millis();
+  const unsigned long timeout = 10;
+
+  while (!DW1000Ng::isTransmitDone())
   {
-    collectMeta();
-    printAndClearMetadataBuffer();
+    if (millis() - startTime > timeout)
+    {
+      DW1000Ng::clearTransmitStatus();
+      return;
+    }
+    yield();
+  }
+
+  DW1000Ng::clearTransmitStatus();
+};
+
+void receiveWaveMessage(WaveMessage &message)
+{
+  DW1000Ng::startReceive();
+  unsigned long startTime = millis();
+  const unsigned long timeout = 10;
+
+  while (!DW1000Ng::isReceiveDone())
+  {
+    if (millis() - startTime > timeout)
+    {
+      DW1000Ng::clearReceiveStatus();
+      DW1000Ng::clearReceiveFailedStatus();
+      DW1000Ng::clearReceiveTimeoutStatus();
+      return;
+    }
+    yield();
+  }
+
+  DW1000Ng::clearReceiveStatus();
+  uint8_t buffer[128];
+  DW1000Ng::getReceivedData(buffer, sizeof(buffer));
+  size_t length = sizeof(buffer);
+  deserializeWaveMessage(buffer, length, message);
+}
+
+// Serialize WaveMessage to byte array
+void serializeWaveMessage(const WaveMessage &message, uint8_t *buffer, size_t &length)
+{
+  buffer[0] = static_cast<uint8_t>(message.type);
+  size_t offset = 1;
+
+  switch (message.type)
+  {
+  case WaveType::BLINK:
+    memcpy(buffer + offset, message.blink.euid.c_str(), message.blink.euid.length() + 1); // +1 for null terminator
+    offset += message.blink.euid.length() + 1;
+    memcpy(buffer + offset, &message.blink.departure_time, sizeof(message.blink.departure_time));
+    offset += sizeof(message.blink.departure_time);
+    buffer[offset++] = static_cast<uint8_t>(message.blink.carrier);
+    break;
+
+  case WaveType::ANSWER:
+    memcpy(buffer + offset, message.answer.euid.c_str(), message.answer.euid.length() + 1);
+    offset += message.answer.euid.length() + 1;
+    memcpy(buffer + offset, message.answer.initiator_euid.c_str(), message.answer.initiator_euid.length() + 1);
+    offset += message.answer.initiator_euid.length() + 1;
+    memcpy(buffer + offset, &message.answer.departure_time, sizeof(message.answer.departure_time));
+    offset += sizeof(message.answer.departure_time);
+    memcpy(buffer + offset, &message.answer.taxi_time, sizeof(message.answer.taxi_time));
+    offset += sizeof(message.answer.taxi_time);
+    buffer[offset++] = static_cast<uint8_t>(message.answer.carrier);
+    break;
+
+  case WaveType::RESULT:
+    memcpy(buffer + offset, message.result.initiator_euid.c_str(), message.result.initiator_euid.length() + 1);
+    offset += message.result.initiator_euid.length() + 1;
+    memcpy(buffer + offset, message.result.responder_euid.c_str(), message.result.responder_euid.length() + 1);
+    offset += message.result.responder_euid.length() + 1;
+    memcpy(buffer + offset, &message.result.calculated_distance, sizeof(message.result.calculated_distance));
+    offset += sizeof(message.result.calculated_distance);
+    break;
+  }
+
+  length = offset;
+}
+
+// Deserialize byte array to WaveMessage
+void deserializeWaveMessage(const uint8_t *buffer, size_t length, WaveMessage &message)
+{
+  message.type = static_cast<WaveType>(buffer[0]);
+  size_t offset = 1;
+
+  // First, destroy the old object inside the union (since union does not automatically manage destruction)
+  switch (message.type)
+  {
+  case WaveType::BLINK:
+    new (&message.blink) BlinkWave(); // Placement new to properly initialize
+    message.blink.euid = String((char *)(buffer + offset));
+    offset += message.blink.euid.length() + 1; // +1 for null terminator
+    memcpy(&message.blink.departure_time, buffer + offset, sizeof(message.blink.departure_time));
+    offset += sizeof(message.blink.departure_time);
+    message.blink.carrier = static_cast<CarrierType>(buffer[offset++]);
+    break;
+
+  case WaveType::ANSWER:
+    new (&message.answer) AnswerWave();
+    message.answer.euid = String((char *)(buffer + offset));
+    offset += message.answer.euid.length() + 1;
+    message.answer.initiator_euid = String((char *)(buffer + offset));
+    offset += message.answer.initiator_euid.length() + 1;
+    memcpy(&message.answer.departure_time, buffer + offset, sizeof(message.answer.departure_time));
+    offset += sizeof(message.answer.departure_time);
+    memcpy(&message.answer.taxi_time, buffer + offset, sizeof(message.answer.taxi_time));
+    offset += sizeof(message.answer.taxi_time);
+    message.answer.carrier = static_cast<CarrierType>(buffer[offset++]);
+    break;
+
+  case WaveType::RESULT:
+    new (&message.result) ResultWave();
+    message.result.initiator_euid = String((char *)(buffer + offset));
+    offset += message.result.initiator_euid.length() + 1;
+    message.result.responder_euid = String((char *)(buffer + offset));
+    offset += message.result.responder_euid.length() + 1;
+    memcpy(&message.result.calculated_distance, buffer + offset, sizeof(message.result.calculated_distance));
+    offset += sizeof(message.result.calculated_distance);
+    break;
   }
 }
